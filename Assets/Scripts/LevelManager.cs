1bcebfc981eaa2e81a7ec70074f1c733c4f5d5b2
fabidalacqua <fabiana.dalacqua@gmail.com>
2018-01-12 16:46:22 -0200

    Implementação do menu

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour {
+
+    public GameObject menu;
+    public GameObject credits;
+
+    public void LoadScreenMenu() {
+        menu.SetActive(true);
+        credits.SetActive(false);
+    }
+    
+    public void LoadScreenCredits() {
+        menu.SetActive(false);
+        credits.SetActive(true);
+    }
+
+    public void LoadScene(string name) {
+        SceneManager.LoadScene(name);
+    }
+}
