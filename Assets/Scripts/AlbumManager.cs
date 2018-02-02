f2700ce06ed16da3e0b5038aaf7ce4f0e9eaaf16
fabidalacqua <fabiana.dalacqua@gmail.com>
2018-02-02 10:15:50 -0200

    Implementação de botões álbum (virar a página) com som

---
+++ Assets/Scripts/AlbumManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AlbumManager : MonoBehaviour {
+
+    public AudioSource bookFlip;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void LoadImages(Button button) {
+        //if(tem mais paginas)
+
+        bookFlip.Play();
+        //else nao carrega mais paginas
+    }
+
+}
