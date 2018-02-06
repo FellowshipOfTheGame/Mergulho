579ce20c24bd75bc36949e54268f581ce50e8ac1
fabidalacqua <fabiana.dalacqua@gmail.com>
2018-02-06 16:29:52 -0200

    Exemplo de Quiz carregado via JSON melhorado

---
+++ Assets/Scripts/AnswerButton.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class AnswerButton : MonoBehaviour 
+{
+	public Text answerText;
+
+	private QuizController gameController;
+	private AnswerData answerData;
+
+	void Start()
+	{
+		gameController = FindObjectOfType<QuizController>();
+	}
+
+	public void SetUp(AnswerData data)
+	{
+		answerData = data;
+        answerText.text = answerData.answerText;
+	}
+
+	public void HandleClick()
+	{
+		gameController.AnswerButtonClicked(answerData.isCorrect);
+	}
+}
--- Assets/Scripts/QuizController.cs
+++ Assets/Scripts/QuizController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class QuizController : MonoBehaviour {
@@ -16,6 +17,11 @@ public class QuizController : MonoBehaviour {
     private RoundData currentRoundData;
     private QuestionData[] questionPool;
 
+    public SimpleObjectPool answerButtonObjectPool;
+    public Transform answerButtonParent;
+
+    private List<GameObject> answerButtonGameObjects = new List<GameObject>();
+
     private int questionIndex;
 
     // Use this for initialization
@@ -29,26 +35,26 @@ public class QuizController : MonoBehaviour {
 
         currentRoundData = dataController.GetCurrentRoundData();
         // Ask the DataController for the data for the current round. At the moment, 
-        //we only have one round - but we could extend this
+        // we only have one round - but we could extend this
 
         questionPool = currentRoundData.questions;
         // Take a copy of the questions so we could shuffle the pool or drop questions from it 
-        //without affecting the original RoundData object
+        // without affecting the original RoundData object
 
         ShowQuestion();
     }
 
     void ShowQuestion()
     {
-        //RemoveAnswerButtons();
+        RemoveAnswerButtons();
 
         QuestionData questionData = questionPool[questionIndex];                            
         // Get the QuestionData for the current question
-        questionText.text = questionData.questionText;                                      
+        questionText.text = questionData.questionText;
         // Update questionText with the correct text
 
-        /*for (int i = 0; i < questionData.answers.Length; i++)                              
-         // For every AnswerData in the current QuestionData...
+        for (int i = 0; i < questionData.answers.Length; i++)                              
+        // For every AnswerData in the current QuestionData...
         {
             GameObject answerButtonGameObject = answerButtonObjectPool.GetObject();         
             // Spawn an AnswerButton from the object pool
@@ -57,8 +63,26 @@ public class QuizController : MonoBehaviour {
             answerButtonGameObject.transform.localScale = Vector3.one;
 
             AnswerButton answerButton = answerButtonGameObject.GetComponent<AnswerButton>();
-            answerButton.SetUp(questionData.answers[i]);                                    // Pass the AnswerData to the AnswerButton so the AnswerButton knows what text to display and whether it is the correct answer
-        }*/
+            answerButton.SetUp(questionData.answers[i]);                                    
+            // Pass the AnswerData to the AnswerButton so the AnswerButton knows what text to display and whether it is the correct answer
+        }
+    }
+
+    void RemoveAnswerButtons()
+    {
+        while (answerButtonGameObjects.Count > 0)                                            // Return all spawned AnswerButtons to the object pool
+        {
+            answerButtonObjectPool.ReturnObject(answerButtonGameObjects[0]);
+            answerButtonGameObjects.RemoveAt(0);
+        }
+    }
+
+    public void AnswerButtonClicked(bool isCorrect)
+    {
+        if (isCorrect)
+        {
+            SceneManager.LoadScene("Game");
+        }
     }
 
     public void ShowInfoScreen(bool show)
