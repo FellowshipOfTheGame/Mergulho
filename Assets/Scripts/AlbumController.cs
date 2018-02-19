35ad031d79d7ada03119763e1a27f88ca0022fd4
fabidalacqua <fabiana.dalacqua@gmail.com>
2018-02-19 13:00:00 -0300

    Correção de navegação do album com adição de imagens de teste

--- Assets/Scripts/AlbumController.cs
+++ Assets/Scripts/AlbumController.cs
@@ -10,71 +10,99 @@ public class AlbumController : MonoBehaviour
     public AudioSource bookFlip;
     public GameObject RightArrow;
     public GameObject LeftArrow;
-    public GameObject imagesContainer;
+    public GameObject page1;
+    public GameObject page2;
+    public GameObject frames2;
 
     private DataController dataController;
     private QuestionData questionData;
-    private Image[] images;
+    private Image[] imgsPage1;
+    private Image[] imgsPage2;
+    private Sprite emptySprite;
+    private Sprite newSprite;
+
     private int questionsLength;
     private int pages;
-    private int currentPage;
+    private int currentPages;
 
     private void Start () {
         dataController = FindObjectOfType<DataController>();
 
-        images = imagesContainer.GetComponentsInChildren<Image>();
+        imgsPage1 = page1.GetComponentsInChildren<Image>();
+        imgsPage2 = page2.GetComponentsInChildren<Image>();
 
         questionsLength = dataController.GetQuestionDataLengh();
 
-        pages = (int)(questionsLength / images.Length);
-        
-        pages = 3;//-----remover
+        pages = Mathf.CeilToInt(questionsLength / 8f);
 
-        currentPage = 1;
+        currentPages = 1;
+
+        emptySprite = Resources.Load<Sprite>("emptySprite");
 
         TurnPage(0);
 	}
 
     private void Update()
     {
-        if (currentPage == pages)
+        if (currentPages == pages)
             RightArrow.SetActive(false);
         else
             RightArrow.SetActive(true);
 
-        if (currentPage == 1)
+        if (currentPages == 1)
             LeftArrow.SetActive(false);
         else
             LeftArrow.SetActive(true);
+    }
+
+    private void LoadImages(int index, int length, Image[] imgs)
+    {
+        for (int i = 0; index < length; i++)
+        {
+            if (index < questionsLength) {
+                frames2.SetActive(true);
+
+                questionData = dataController.GetQuestionData(index);
+
+                if (questionData.wasAnswered)
+                {
+                    newSprite = Resources.Load<Sprite>("Collectible/" + index);
+
+                    imgs[i].sprite = newSprite;
+                    imgs[i].color = Color.white;
+                }
+                else
+                {
+                    imgs[i].sprite = emptySprite;
+                    imgs[i].color = Color.clear;
+                }
+            }
+            else
+            {
+                frames2.SetActive(false);
+                imgs[i].sprite = emptySprite;
+                imgs[i].color = Color.clear;
+            }
+
+            index++;
+        }
     }
 
     public void TurnPage(int dir) {
         bookFlip.Play();
 
         if (dir == 1)
-            currentPage++;
+            currentPages++;
         else if(dir == -1)
-            currentPage--;
+            currentPages--;
 
-        int imagesPerPage = images.Length * currentPage;
-        int index = images.Length - imagesPerPage;
+        int index = 8 * (currentPages - 1);
+        int length = 8 * currentPages;
 
         if (index < 0)
             index *= -1;
 
-        while (index < questionsLength)
-        {
-            questionData = dataController.GetQuestionData(index);
-
-            if (questionData.wasAnswered)
-            {
-                Sprite obj = Resources.Load<Sprite>("Collectible/" + index);
-
-                images[index].sprite = obj;
-                images[index].color = Color.white;
-            }
-
-            index++;
-        }
+        LoadImages(index, length - 4, imgsPage1);
+        LoadImages(index + 4, length, imgsPage2);
     }
 }
