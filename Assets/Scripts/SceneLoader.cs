204f7360a245e08667547d48369e438a1409b09f
fabidalacqua <fabiana.dalacqua@gmail.com>
2018-03-18 18:42:34 -0300

    Tela de loading, novos prefabs, mudança de fonte

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -1,14 +1,25 @@
 ﻿using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class SceneLoader : MonoBehaviour
 {
     public string sceneName;
+    public GameObject sprite;
 
-    void Update()
+    private SpriteRenderer spriteRenderer;
+
+    private void Start()
     {
         StartCoroutine(LoadNewScene());
+    }
+
+    private void Update()
+    {
+        /*spriteRenderer = sprite.GetComponent<SpriteRenderer>();
+        spriteRenderer.color = new Color(spriteRenderer.color.r, spriteRenderer.color.g, spriteRenderer.color.b, Mathf.PingPong(Time.time, 1f));
+        */
     }
 
     // The coroutine runs on its own at the same time as Update() and takes an integer indicating which scene to load.
